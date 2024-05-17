06c2bc618fdb1ae77f5a0710ed000c419ba87c21
spl1ntero <sernekit77@gmail.com>
2024-05-17 14:04:00 +0300

    сделал что-то

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ import json
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Замените на ваш секретный ключ
 
-
 def load_data(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
-
 
 def save_data(data, filename):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
-
 flowers = load_data('flowers.json')
 suppliers = load_data('suppliers.json')
 sellers = load_data('sellers.json')
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -30,7 +26,6 @@ def index():
     else:
         return render_template('index.html', flowers=flowers, current_season=None, suppliers=suppliers, sellers=sellers)
 
-
 @app.route('/add_info', methods=['GET'])
 def add_info():
     if 'username' in session and session['username'] == 'admin':
@@ -39,28 +34,23 @@ def add_info():
         flash('Пожалуйста, войдите для доступа к админ-панели.')
         return redirect(url_for('login'))
 
-
 @app.route('/get_flowers', methods=['GET'])
 def get_flowers():
     return jsonify(flowers)
-
 
 @app.route('/get_suppliers', methods=['GET'])
 def get_suppliers():
     return jsonify(suppliers)
 
-
 @app.route('/get_sellers', methods=['GET'])
 def get_sellers():
     return jsonify(sellers)
-
 
 @app.route('/filter_by_season', methods=['GET'])
 def filter_by_season():
     season = request.args.get('season')
     filtered_flowers = [flower for flower in flowers if flower['season'] == season]
     return render_template('index.html', flowers=filtered_flowers, suppliers=suppliers, sellers=sellers)
-
 
 @app.route('/filter_by_country', methods=['GET'])
 def filter_by_country():
@@ -69,14 +59,11 @@ def filter_by_country():
     return render_template('index.html', flowers=filtered_flowers, current_season=None, suppliers=suppliers,
                            sellers=sellers)
 
-
 @app.route('/filter_by_supplier', methods=['GET'])
 def filter_by_supplier():
     name = request.args.get('name')
     filtered_suppliers = [supplier for supplier in suppliers if supplier['name'] == name]
     return render_template('index.html', flowers=flowers, current_season=None, suppliers=suppliers, filtered_suppliers=filtered_suppliers, sellers=sellers)
-
-
 
 @app.route('/add_seller', methods=['POST'])
 def add_seller():
@@ -94,7 +81,6 @@ def add_seller():
         flash('Недостаточно прав.')
         return redirect(url_for('login'))
 
-
 @app.route('/add_supplier', methods=['POST'])
 def add_supplier():
     if 'username' in session and session['username'] == 'admin':
@@ -111,7 +97,6 @@ def add_supplier():
     else:
         flash('Недостаточно прав.')
         return redirect(url_for('login'))
-
 
 @app.route('/add_flower', methods=['POST'])
 def add_flower():
@@ -144,7 +129,6 @@ def add_flower():
         flash('Недостаточно прав.')
         return redirect(url_for('login'))
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -157,12 +141,10 @@ def login():
             flash('Неправильное имя пользователя или пароль.')
     return render_template('login.html')
 
-
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
-
 
 @app.route('/flowers_by_supplier', methods=['GET'])
 def flowers_by_supplier():
@@ -190,7 +172,6 @@ def common_suppliers():
     suppliers_seller2 = {flower['supplier'] for flower in flowers if flower['seller'] == seller2}
     common_suppliers = list(suppliers_seller1.intersection(suppliers_seller2))
     return render_template('index.html', flowers=flowers, current_season=None, suppliers=common_suppliers, sellers=sellers, filtered_suppliers=None)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
